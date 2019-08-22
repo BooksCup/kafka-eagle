7f5180917ae5ab293a88a5032f8e4f889773b965
BooksCup <812809680@qq.com>
2019-08-22 11:16:24 +0800

    test fork commit

--- kafka-eagle-common/src/main/java/org/smartloli/kafka/eagle/common/protocol/AlarmInfo.java
+++ kafka-eagle-common/src/main/java/org/smartloli/kafka/eagle/common/protocol/AlarmInfo.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,57 +19,57 @@ package org.smartloli.kafka.eagle.common.protocol;
 
 /**
  * Definition alarmer information.
- * 
+ *
  * @author smartloli.
  *
  *         Created by Sep 7, 2016
  */
-public class AlarmInfo extends BaseProtocol{
+public class AlarmInfo extends BaseProtocol {
 
-	private String group = "";
-	private String topics = "";
-	private long lag = 0L;
-	private String owners = "";
-	private String modifyDate = "";
+    private String group = "";
+    private String topics = "";
+    private long lag = 0L;
+    private String owners = "";
+    private String modifyDate = "";
 
-	public String getGroup() {
-		return group;
-	}
+    public String getGroup() {
+        return group;
+    }
 
-	public void setGroup(String group) {
-		this.group = group;
-	}
+    public void setGroup(String group) {
+        this.group = group;
+    }
 
-	public String getTopics() {
-		return topics;
-	}
+    public String getTopics() {
+        return topics;
+    }
 
-	public void setTopics(String topics) {
-		this.topics = topics;
-	}
+    public void setTopics(String topics) {
+        this.topics = topics;
+    }
 
-	public String getOwners() {
-		return owners;
-	}
+    public String getOwners() {
+        return owners;
+    }
 
-	public void setOwners(String owners) {
-		this.owners = owners;
-	}
+    public void setOwners(String owners) {
+        this.owners = owners;
+    }
 
-	public long getLag() {
-		return lag;
-	}
+    public long getLag() {
+        return lag;
+    }
 
-	public void setLag(long lag) {
-		this.lag = lag;
-	}
+    public void setLag(long lag) {
+        this.lag = lag;
+    }
 
-	public String getModifyDate() {
-		return modifyDate;
-	}
+    public String getModifyDate() {
+        return modifyDate;
+    }
 
-	public void setModifyDate(String modifyDate) {
-		this.modifyDate = modifyDate;
-	}
+    public void setModifyDate(String modifyDate) {
+        this.modifyDate = modifyDate;
+    }
 
 }
