575d5d212072bdc13e24c9584ce50b5d2ca986e3
BooksCup <812809680@qq.com>
2019-08-22 11:17:50 +0800

    test fork commit

--- kafka-eagle-common/src/main/java/org/smartloli/kafka/eagle/common/protocol/AlertInfo.java
+++ kafka-eagle-common/src/main/java/org/smartloli/kafka/eagle/common/protocol/AlertInfo.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,84 +19,84 @@ package org.smartloli.kafka.eagle.common.protocol;
 
 /**
  * Definition AlertInfo information.
- * 
+ *
  * @author smartloli.
  *
  *         Created by Oct 27, 2018
  */
 public class AlertInfo extends BaseProtocol {
 
-	private int id;
-	private String group = "";
-	private String cluster = "";
-	private String topic = "";
-	private long lag = 0L;
-	private String owner = "";
-	private String created = "";
-	private String modify = "";
+    private int id;
+    private String group = "";
+    private String cluster = "";
+    private String topic = "";
+    private long lag = 0L;
+    private String owner = "";
+    private String created = "";
+    private String modify = "";
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getGroup() {
-		return group;
-	}
+    public String getGroup() {
+        return group;
+    }
 
-	public void setGroup(String group) {
-		this.group = group;
-	}
+    public void setGroup(String group) {
+        this.group = group;
+    }
 
-	public String getCluster() {
-		return cluster;
-	}
+    public String getCluster() {
+        return cluster;
+    }
 
-	public void setCluster(String cluster) {
-		this.cluster = cluster;
-	}
+    public void setCluster(String cluster) {
+        this.cluster = cluster;
+    }
 
-	public String getTopic() {
-		return topic;
-	}
+    public String getTopic() {
+        return topic;
+    }
 
-	public void setTopic(String topic) {
-		this.topic = topic;
-	}
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
 
-	public long getLag() {
-		return lag;
-	}
+    public long getLag() {
+        return lag;
+    }
 
-	public void setLag(long lag) {
-		this.lag = lag;
-	}
+    public void setLag(long lag) {
+        this.lag = lag;
+    }
 
-	public String getOwner() {
-		return owner;
-	}
+    public String getOwner() {
+        return owner;
+    }
 
-	public void setOwner(String owner) {
-		this.owner = owner;
-	}
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 
-	public String getCreated() {
-		return created;
-	}
+    public String getCreated() {
+        return created;
+    }
 
-	public void setCreated(String created) {
-		this.created = created;
-	}
+    public void setCreated(String created) {
+        this.created = created;
+    }
 
-	public String getModify() {
-		return modify;
-	}
+    public String getModify() {
+        return modify;
+    }
 
-	public void setModify(String modify) {
-		this.modify = modify;
-	}
+    public void setModify(String modify) {
+        this.modify = modify;
+    }
 
 }
